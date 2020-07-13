6a80ce60a35791378d19df3a04195bdee48f9e67
ThiagoDT <galaspion5td@gmail.com>
2020-07-13 20:46:31 -0300

    controller usuario

--- Calc/src/main/java/controller/UsuarioController.java
+++ Calc/src/main/java/controller/UsuarioController.java
@@ -6,6 +6,7 @@
 package controller;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -13,33 +14,36 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Thiago
  */
-public class UsuarioController implements IController{
-
+public class UsuarioController extends HttpServlet{
+    
     @Override
-    public void doVerfirc(HttpServletRequest req, HttpServletResponse res,ServletContext sc) {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp){
         try{
+            ServletContext sc = req.getServletContext();
+            resp.setCharacterEncoding("UTF-8");
+            resp.setContentType("text/html");
+            
             req.setCharacterEncoding("UTF-8");
             
             String email = req.getParameter("email");
             String senha = req.getParameter("senha");
             
             if(email.equals("teste@teste.com")&&senha.equals("senha")){
-                sc.getRequestDispatcher("/jsp/calculadora.jsp").forward(req, res);  
+                sc.getRequestDispatcher("/Templates/calculadora.jsp").forward(req, resp);  
             }else{
                 req.setAttribute("messagem", "Usuario Invalido");
-                sc.getRequestDispatcher("/jsp/login.jsp").forward(req, res);
+                sc.getRequestDispatcher("/Templates/login.jsp").forward(req, resp);
             }
-            
-            
-        }catch(NullPointerException ne){}
-        catch(Exception ex){
-            System.err.println("ERRO: "+ex.getMessage()+":\n"+ex.getLocalizedMessage());
-        } 
+        }catch(Exception ex){}
     }
-
+    
     @Override
-    public void doAdd(HttpServletRequest req, HttpServletResponse res, ServletContext sc) {
+    public void doPost(HttpServletRequest req, HttpServletResponse resp){
         try{
+            ServletContext sc = req.getServletContext();
+            resp.setCharacterEncoding("UTF-8");
+            resp.setContentType("text/html");
+            
             req.setCharacterEncoding("UTF-8");
             String email = req.getParameter("email");
             String nome= req.getParameter("nome");
@@ -51,45 +55,11 @@ public class UsuarioController implements IController{
             nome=nome.trim().replace(" ", "");
             
             if(!nome.isEmpty() && !senha.isEmpty() && !confSenha.isEmpty()){
-                sc.getRequestDispatcher("/jsp/calculadora.jsp").forward(req, res);  
+                sc.getRequestDispatcher("/Templates/calculadora.jsp").forward(req, resp);  
             }else{
                 req.setAttribute("mensagem", "Campo vazio");
-                sc.getRequestDispatcher("/jsp/criarConta.jsp").forward(req, res);
+                sc.getRequestDispatcher("/Templates/criarConta.jsp").forward(req, resp);
             }
-            
-        }catch(NullPointerException ne){}
-        catch(Exception ex){
-            System.err.println("ERRO: "+ex.getMessage()+":\n"+ex.getLocalizedMessage());
-        }
+        }catch(Exception ex){}
     }
-
-    @Override
-    public void doUpdate(HttpServletRequest req, HttpServletResponse res, ServletContext sc) {
-try{
-            req.setCharacterEncoding("UTF-8");
-            String email = req.getParameter("email");
-            String nome= req.getParameter("nome");
-            String senha= req.getParameter("senha");
-            
-            senha=senha.trim().replace(" ", "");
-            nome=nome.trim().replace(" ", "");
-            
-            if(!nome.isEmpty() && !senha.isEmpty()){
-                req.setAttribute("mensagem", "Atualizado");
-            }else{
-                req.setAttribute("mensagem", "Campo vazio");
-            }
-            sc.getRequestDispatcher("/jsp/historico.jsp").forward(req, res);
-            
-        }catch(NullPointerException ne){}
-        catch(Exception ex){
-            System.err.println("ERRO: "+ex.getMessage()+":\n"+ex.getLocalizedMessage());
-        }    
-    }
-
-    @Override
-    public void doDelete(HttpServletRequest req, HttpServletResponse res, ServletContext sc) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
 }
