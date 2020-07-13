a7220ba329b74bf9dfbf3b2184059bafd5dfc153
ThiagoDT <galaspion5td@gmail.com>
2020-07-13 20:45:47 -0300

    operações da calculadora

---
+++ Calc/src/main/java/outros/Dividir.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package outros;
+
+/**
+ *
+ * @author Thiago
+ */
+public class Dividir implements IOperador{
+
+    @Override
+    public String resultado(float n1, float n2) {
+        if(n2>0){
+            float r=n1/n2;
+            return String.valueOf(r);
+        }
+        return String.valueOf("Valor incorreto");
+    }
+    
+}
---
+++ Calc/src/main/java/outros/Subtrair.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package outros;
+
+/**
+ *
+ * @author Thiago
+ */
+public class Subtrair implements IOperador{
+
+    @Override
+    public String resultado(float n1, float n2) {
+        float r=n1-n2;
+        return String.valueOf(r);
+    }
+    
+}
