5ae6b805ab5a0acc6b24507504eaffc5c53d9a7a
ThiagoDT <galaspion5td@gmail.com>
2020-07-13 21:08:09 -0300

    classe com função de divisão

--- Calc/src/main/java/outros/Dividir.java
+++ Calc/src/main/java/outros/Dividir.java
@@ -13,7 +13,7 @@ public class Dividir implements IOperador{
 
     @Override
     public String resultado(float n1, float n2) {
-        if(n2>0){
+        if(n2!=0){
             float r=n1/n2;
             return String.valueOf(r);
         }
